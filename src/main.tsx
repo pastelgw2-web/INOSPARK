6ed500acaeb8c59db0715fc579ef804a27d59ca5
pastelgw2-web <pastelgw2@gmail.com>
2026-02-09 15:49:08 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // Menambahkan ekstensi .tsx agar lebih aman bagi Vite
+import App from './App'; 
 
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to. Make sure index.html has <div id='root'></div>");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
